b02b3e03a5b69a5dae83197ae578ecd1e4237b24
lamngx2808 <81542586+lamngx2808@users.noreply.github.com>
2021-03-29 11:28:02 +0700

    Add files via upload

---
+++ hello1.c
@@ -0,0 +1,13 @@
+/* Mguyen Thanh lam – VN-02 */
+/* This is my second program in C */
+#include <stdio.h>
+main()
+{
+printf("Welcome to C.\n");
+printf("Programming Introduction.\n");
+printf("*************************\n");
+printf("My name is Thanh Lam.\n ");
+printf("Nice to meet you.\n");
+printf("Hope you wwil have a happy time.\n");
+printf("*************************\n");
+}
